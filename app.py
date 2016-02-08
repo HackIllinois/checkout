0b7a527c64a10b9f84b990d766ba0d50b68fecec
Rohan Mathur <rohan@rmathur.com>
2016-02-07 19:42:39 -0500

    Add deployment stuff
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- app.py
+++ app.py
@@ -127,6 +127,5 @@ def return_item():
     return jsonify(hacker_json)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
     db.init_app(app)
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
