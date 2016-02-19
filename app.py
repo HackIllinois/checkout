ce5071ab21794340bf13f4c9a7b6c504b82aa477
Rohan Mathur <rohan@rmathur.com>
2016-02-19 12:50:49 -0500

    Deployment updates
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- app.py
+++ app.py
@@ -3,12 +3,60 @@ from flask import Flask, abort, jsonify, request
 from models import db, Item, Hacker
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
 from config import SECRET_KEY
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+	f.required_methods = ['OPTIONS']
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
 @app.route('/items', methods=['GET'])
+@crossdomain(origin='*')
 def all_items():
     items = Item.query.all()
     items_json = []
@@ -21,6 +69,7 @@ def all_items():
     return jsonify(items=items_json)
 
 @app.route('/hacker/<int:hacker_barcode>', methods=['GET'])
+@crossdomain(origin='*')
 def hacker_items(hacker_barcode):
     hacker = Hacker.query.filter_by(barcode=hacker_barcode).first()
     if not hacker:
@@ -34,6 +83,7 @@ def hacker_items(hacker_barcode):
     return jsonify(hacker_json)
 
 @app.route('/items/new', methods=['POST'])
+@crossdomain(origin='*')
 def new_item():
     secret = request.form['secret']
     if secret != SECRET_KEY:
@@ -56,6 +106,7 @@ def new_item():
     return jsonify(item_json)
 
 @app.route('/items/checkout', methods=['POST'])
+@crossdomain(origin='*')
 def checkout_item():
     secret = request.form['secret']
     if secret != SECRET_KEY:
@@ -98,6 +149,7 @@ def checkout_item():
     return jsonify(info=ret_json)
 
 @app.route('/items/return', methods=['POST'])
+@crossdomain(origin='*')
 def return_item():
     secret = request.form['secret']
     if secret != SECRET_KEY:
